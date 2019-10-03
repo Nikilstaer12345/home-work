41bc20a1160ad0d47cb9a31b40c564c32f934bd9
Nikilstaer12345 <55661274+Nikilstaer12345@users.noreply.github.com>
2019-10-03 20:10:01 +0300

    Add files via upload

---
+++ Задача №4.cpp
@@ -0,0 +1,34 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{setlocale(LC_ALL,"russian");
+{int a, b;
+cout <<"До замены значения переменных:" << endl;
+cout << "a=";
+cin >> a;
+cout << "b=";
+cin >> b;
+a = a + b;
+b = a - b;
+a = a - b;
+cout << "после замены:" << endl;
+cout << "a=" << a << "\n" << "b=" << b<<"\n";
+}
+{ 
+	int a, b,c;
+cout << "До замены значения переменных:" << endl;
+cout << "a=";
+cin >> a;
+cout << "b=";
+cin >> b;
+c = a;
+a = b;
+b = c;
+cout << "после замены:" << endl;
+cout << "a=" << a << "\n" << "b=" << b;
+}
+
+}
+
---
+++ Задача №5.cpp
@@ -0,0 +1,18 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+	double v0, x0, xt1,xt2, t, g = -9.8,a;
+	a = g;
+	cout << "v0=";
+	cin >> v0;
+	cout << "x0=";
+	cin >> x0;
+	cout << "t=";
+	cin >> t;
+	xt1 = x0 + v0 * t + a * t * t / 2;
+	cout << "xt1=" << xt1 << endl;
+	xt2 = x0 + v0 * t + 1 / 2 * a * t * t;
+	cout << "xt2=" << xt2 << endl;
+}
