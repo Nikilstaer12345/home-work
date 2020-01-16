0048e6a1387c57c1100c05c624aa4561a80c7d2e
Nikilstaer12345 <55661274+Nikilstaer12345@users.noreply.github.com>
2020-01-16 22:25:17 +0300

    Add files via upload

---
+++ Задача №8.cpp
@@ -0,0 +1,40 @@
+﻿// Задача №8.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Ru");
+	double a, b;
+	char rt = 'a';
+	cout << "Введите пример" << endl;
+	cin >> a;
+	cin >> rt;
+	cin >> b;
+	switch (rt) {
+	case '+':
+		cout << a + b;
+		break;
+	case '-':
+		cout << a - b;
+		//break;
+	case '*':
+		cout << a * b;
+		break;
+	case '/':
+		if (b == 0) {
+			cout << "невозможно деление на ноль" << endl;
+		}
+		else
+			cout << a / b;
+		break;
+
+	default :
+		cout << "Недопустимая операция";
+		break;
+	}
+
+
+}
